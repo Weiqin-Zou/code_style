3accc73f55392db6c3260ff671a4555620ce36c4
zouxiaoyu <weiqinzou@qq.com>
2016-01-14 13:51:32 +0800

    exception handled

--- filter_FWM_event.py
+++ filter_FWM_event.py
@@ -14,34 +14,46 @@ def filter_FWM_event(hist_json_fin,FWM_fout,abnormalHist_fout):
         repoKey2="repository"
         for hist in hist_json_fin.xreadlines():
             try:
-                hist=json.loads(hist)
+                try:
+                    tmphist=hist
+                    hist=json.loads(hist)
+                except:
+                    print tmphist
+                    continue
+
                 typeEvent=hist["type"]
                 if typeEvent in ("ForkEvent","WatchEvent","MemberEvent"):
-                    eventTime=hist["created_at"]
-                    if repoKey1 in hist:
-                        repoUrl=hist[repoKey1]["url"]
-                        if typeEvent in ("ForkEvent","WatchEvent"):
-                            who=hist["actor"]["login"]
-                    if repoKey2 in hist:
-                        repoUrl=hist[repoKey2]["url"]
-                        if typeEvent in ("ForkEvent","WatchEvent"):
-                            who=hist["actor"]
-                    if typeEvent=="ForkEvent":
-                        action="Fork"
-                    elif typeEvent=="WatchEvent":
-                        action="Watch"
-                    else:
-                        who=hist["payload"]["member"]["login"]
-                        action=hist["payload"]["action"]
+                    try:
+                        eventTime=hist["created_at"]
+                        if repoKey1 in hist:
+                            repoUrl=hist[repoKey1]["url"]
+                            if typeEvent in ("ForkEvent","WatchEvent"):
+                                who=hist["actor"]["login"]
+                        if repoKey2 in hist:
+                            repoUrl=hist[repoKey2]["url"]
+                            if typeEvent in ("ForkEvent","WatchEvent"):
+                                who=hist["actor"]
+                        if typeEvent=="ForkEvent":
+                            action="Fork"
+                        elif typeEvent=="WatchEvent":
+                            action="Watch"
+                        else:
+                            try:
+                                who=hist["payload"]["member"]["login"]
+                            except:
+                                who=hist["payload"]["member"]
+                            action=hist["payload"]["action"]
 
-                    repoName="/".join(repoUrl.split('/')[-2:])
+                        repoName="/".join(repoUrl.split('/')[-2:])
 
-                    print >>FWM_fout,"%s,%s,%s,%s,%s" % (repoName,typeEvent,eventTime,who,action)
+                        print >>FWM_fout,"%s,%s,%s,%s,%s" % (repoName,typeEvent,eventTime,who,action)
+                    except:
+                        print >>abnormalHist_fout,tmphist
             except:
-                print >>abnormalHist_fout,hist
                 traceback.print_exc()
     except:
         traceback.print_exc()
+
 if __name__ == '__main__':
     try:
         hist_fin=sys.argv[1]
--- filter_hist.sh
+++ filter_hist.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+#example:./filter_hist.sh 2011 2011 histD res > err.log 2>&1
+
 startYear=$1
 endYear=$2
+histPath=$3 #the historay data path
+resPath=$4
 histStat=${startYear}to${endYear}_jsonCnt
-
+mkdir -p $resPath
 if [ -f "$histStat" ]; then
     rm $histStat
 fi
 
 for year in `seq $startYear $endYear`
 do
-    mkdir -p ./res/${year}
-
-    for hist in ../$year/*.json.gz
+    for hist in ${histPath}/$year/*.json.gz
     do
         timeHist=$(echo ${hist} | awk -F "/" '{print $NF}')
         cp $hist $timeHist
@@ -21,15 +23,21 @@ do
         if [ -f "$ungzipTimeHist" ]; then
         rm $ungzipTimeHist
         fi
+
         gzip -d $timeHist
-        histCnt=$(wc -l $ungzipTimeHist)
-        echo $histCnt >>$histStat
-        if [ $histCnt -eq 0 ];then
+        histCnt=$(echo `wc -l $ungzipTimeHist` | cut -f1 -d " ")
+        if [ $histCnt -eq 0 ]
+        then
             sed -i 's/}{/}\n{/g' $ungzipTimeHist
         fi
-        
+        histCnt=$(wc -l $ungzipTimeHist)
+        echo $histCnt >>$histStat
+        echo "totalCnt of Fork,Watch,Member Event:" \
+            `grep -E "ForkEvent|WatchEvent|MemberEvent" $ungzipTimeHist | wc -l` >>$histStat
+        echo ${ungzipTimeHist}
         python filter_FWM_event.py $ungzipTimeHist ${year}.res ${year}.abnormal.log
-        mv ${year}.res ${year}.abnormal.log ./res/${year}
+        
         rm $ungzipTimeHist
     done
+    mv ${year}.res ${year}.abnormal.log ${resPath} 
 done
