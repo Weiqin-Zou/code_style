d7989a7874e9f802908b669e9749a89814b8f86a
zouxiaoyu <weiqinzou@qq.com>
2016-01-18 11:11:33 +0800

    know how to transform time

--- get_loc.sh
+++ get_loc.sh
@@ -2,6 +2,11 @@
 
 #example:./get_loc.sh mongoIP finished_repolist clientAccount
 
+#timezone related time chansform to unix timestamp
+#date -d "2016-01-18T02:26:35Z" +%s
+#date -d "2016-01-18T10:26:35+08:00" +%s ###run on unix system not mac
+#use git log --date=iso-strict to get the just above 2016-01-18T10:26:35+08:00 time format
+
 mongoIP=$1
 finishedRepoList=$2
 clientFile=$3 #the file contains client id and secret
@@ -19,7 +24,43 @@ python get_newRepo_closedPR.py $mongoIP $finishedRepoList full_repoList newRepo_
 }
 #get_newRepo_closedPR 
 
+function get_mostRecentCommit(){
+
+reposDir=repos
+mkdir -p $repoDir
+for fn in $(cat newRepo_list)
+do
+    #clone the new repo
+    repo=$(echo $fn | awk -F "/" '{print $2}')
+    rm -rf $reposDir/$repo
+    cd $reposDir
+    repoUrl="git@github.com:""$fn"".git"
+    git clone $repoUrl
+
+    #get the merged pr and unmerged pr
+    grep "^"$fn"," newRepo_closedPR.res | grep ",merged," > ${repo}/merged
+    grep "^"$fn"," newRepo_closedPR.res | grep ",unmerged," > ${repo}/unmerged
+
+    cd $repo
+    git log | grep -E "^commit |^Date:" > commitLog
+
+    #for merged pr, use the previous commit before the first commit sha of the pr
+    for mpr in $(cat merged)
+    do
+        cmtSha=$(echo $mpr | cut -f4 -d ",")
+        mostRecentSha=$(git log --pretty=oneline $cmtSha"~2"..$cmtSha"~1" | cut -f1 -d " ")
+        echo $mpr,$mostRecentSha >>mpr_recentSha
+    done
+
+    #for closed pr without merged, use the commit time before the created time of the pr
+ 
+done
+}
+
 function cal_loc(){
+git reset $mostRecentSha
+        loc=$(find ./ -name "*.java" | xargs -I {} wc -l {}| cut -f1 -d "." | awk '{sum+=$1}END{print sum}')
+
 reposDir=repos
 mkdir -p $repoDir
 for fn in $(cat newRepo_list)
