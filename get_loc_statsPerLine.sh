302a06f8adb292fc3676c7863d2df86f83db30c8
zouxiaoyu <weiqinzou@qq.com>
2016-01-20 19:55:10 +0800

    loc output to file

--- get_loc_statsPerLine.sh
+++ get_loc_statsPerLine.sh
@@ -7,6 +7,7 @@ newRepoList=$2 #this file contains all new repo' names
 mostRecent=$3 #the dir which contains new repos's all closed pr's most recent commit
 
 function cal_loc(){
+rm newRepo_loc.res
 for fn in $(cat $newRepoList)
 do
     repo=$(echo $fn | awk -F "/" '{print $2}')
@@ -16,7 +17,7 @@ do
         sha=$(echo $pr | cut -f6 -d ",")
         git reset $sha
         loc=$(find ./ -name "*.java" | xargs -I {} wc -l {}| cut -f1 -d "." | awk '{sum+=$1}END{print sum}')
-        echo $pr,$loc
+        echo $pr,$loc>>newRepo_loc.res
     done
     cd ../../
 done
