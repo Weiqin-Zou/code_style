19870b105ce02578c3136f8c81606b97b80e10bd
zouxiaoyu <weiqinzou@qq.com>
2016-01-20 19:46:23 +0800

    cal loc script

---
+++ get_loc_statsPerLine.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#example:./get_loc_statsPerLine.sh newRepo_list reposDir mostRecentShaDir
+
+reposDir=$1 #the dir which contains new repos's code base
+newRepoList=$2 #this file contains all new repo' names
+mostRecent=$3 #the dir which contains new repos's all closed pr's most recent commit
+
+function cal_loc(){
+for fn in $(cat $newRepoList)
+do
+    repo=$(echo $fn | awk -F "/" '{print $2}')
+    cd $reposDir/$repo
+    for pr in $(cat ../../${mostRecent}/${repo}_pr_recentSha)
+    do
+        sha=$(echo $pr | cut -f6 -d ",")
+        git reset $sha
+        loc=$(find ./ -name "*.java" | xargs -I {} wc -l {}| cut -f1 -d "." | awk '{sum+=$1}END{print sum}')
+        echo $pr,$loc
+    done
+    cd ../../
+done
+}
+
+cal_loc $1 $2 $3
