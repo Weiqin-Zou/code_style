292f31fc7c38a68c5fe9e563909fa84e16999855
zouxiaoyu <weiqinzou@qq.com>
2016-01-21 20:23:25 +0800

    let's to get try the stats experiment

--- get_loc_statsPerLine.sh
+++ get_loc_statsPerLine.sh
@@ -6,7 +6,7 @@ reposDir=$1 #the dir which contains new repos's code base
 newRepoList=$2 #this file contains all new repo' names
 mostRecent=$3 #the dir which contains new repos's all closed pr's most recent commit
 
-function cal_loc(){
+function cal_loc_stats(){
 rm newRepo_loc.res
 for fn in $(cat $newRepoList)
 do
@@ -16,11 +16,22 @@ do
     do
         sha=$(echo $pr | cut -f6 -d ",")
         git reset --hard $sha
-        loc=$(find ./ -name "*.java" | xargs -I {} wc -l {}| cut -f1 -d "." | awk '{sum+=$1}END{print sum}')
-        echo $pr,$loc>>../../newRepo_loc.res
+        loc=$(find ./ -name "*.java" | xargs -I {} wc -l {}| cut -f1 -d "." | 
+        awk '{sum+=$1}END{print sum}')
+
+        if [ $loc ];
+        then
+            stats=$(find ./ -name "*.java" | xargs -I {} python statsPerLine.py {} zoutmpFile | 
+            cut -f 2,5 -d " " | 
+            awk '{cnt+=$1;comaCnt+=$2}END{if(cnt==0){print 1}else{print comaCnt*1.0/cnt}}')
+        else 
+            $stats="#"
+        fi
+        echo $pr,$loc,$stats>>../../newRepo_loc_stats.res
+
     done
     cd ../../
 done
 }
 
-cal_loc $1 $2 $3
+cal_loc_stats $1 $2 $3
