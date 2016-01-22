e8ca9ec2eb4e9e522c32f132b35be632150615f1
weiqinzou <weiqinzou@qq.com>
2016-01-22 23:57:13 +0800

    buggy pr heuristic rule set ok

--- get_loc_statsPerLine.sh
+++ get_loc_statsPerLine.sh
@@ -7,7 +7,7 @@ newRepoList=$2 #this file contains all new repo' names
 mostRecent=$3 #the dir which contains new repos's all closed pr's most recent commit
 
 function cal_loc_stats(){
-rm newRepo_loc.res
+rm newRepo_loc_stats.res
 for fn in $(cat $newRepoList)
 do
     repo=$(echo $fn | awk -F "/" '{print $2}')
@@ -21,8 +21,8 @@ do
 
         if [ $loc ];
         then
-            stats=$(find ./ -name "*.java" | xargs -I {} python statsPerLine.py {} zoutmpFile | 
-            cut -f 2,5 -d " " | 
+            stats=$(find ./ -name "*.java" | xargs -I {} python ../../statsPerLine.py {} zoutmpFile | 
+            cut -f2,4 -d " " | 
             awk '{cnt+=$1;comaCnt+=$2}END{if(cnt==0){print 1}else{print comaCnt*1.0/cnt}}')
         else 
             $stats="#"
