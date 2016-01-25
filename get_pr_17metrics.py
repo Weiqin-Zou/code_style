ff7d11b255ccdf1c3c7944dce71eb76e6c5e58b7
zouxiaoyu <weiqinzou@qq.com>
2016-01-25 16:11:56 +0800

    fetch the pr_17metrics

---
+++ get_pr_17metrics.py
@@ -0,0 +1,52 @@
+import traceback
+import os
+import sys
+from  pymongo import *
+
+def get_prMetrics(db,pr_fout=None):
+    try:
+        if not pr_fout:
+            pr_fout=file("pr_17metrics.res",'a')
+
+        '''print pr header'''
+        items=[]
+        header=""
+        for pr in db.repocondition.find({}):
+            try:
+                del pr["_id"]
+                items=pr.keys()
+                for key in items:
+                    if not header:
+                        header=key
+                    else:
+                        header=header+","+key
+                pr_fout.write(header+'\n')
+                break
+            except:
+                traceback.print_exc()
+
+        '''print pr body '''
+        for pr in db.result.find({}):
+            try:
+                rbody=str(pr[items[0]])
+                for i in items[1:]:
+                    rbody=rbody+","+str(pr[i])
+                pr_fout.write(rbody+'\n')
+            except:
+                traceback.print_exc()
+                continue
+    except:
+        traceback.print_exc()
+
+
+if __name__ == '__main__':
+    try:
+        ip=sys.argv[1]
+        client=MongoClient(ip,27017)
+        db=client.Experiment
+        pr_fout=sys.argv[2]
+        get_prMetrics(db,file(pr_fout,'w'))
+        client.close()
+    except:
+        traceback.print_exc()
+
--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 mongoIP=$1
+
+
+#for PR's 18 metrics, actually only 17 from Yiming are obtained, the other one is from me.
+#so this script is used to get the 17 metrics of PR from Yiming.
+python get_pr_17metrics.py $mongoIP pr_17_metrics.res
+
+
 #./run_codeStyle.sh "commit[s|ed|ing]*|fix[es|ed|ing]*" filteredRepo
 
 #python retrieve_gitInfo.py repoList mergedPR $1 
 #cut -f1 -d "," repoList > filteredRepo
-sed '1d' filteredRepo > tmpList 
-./findFixCommit.sh tmpList "fix[es|ed|ing]*"
-
-./isPRbuggy.sh tmpList mergedPR
-
-rm tmpList
-
-./get_loc.sh $mongoIP fini client_IDSecret > log
+#sed '1d' filteredRepo > tmpList 
+#./findFixCommit.sh tmpList "fix[es|ed|ing]*"
+#
+#./isPRbuggy.sh tmpList mergedPR
+#
+#rm tmpList
+#
+#./get_loc.sh $mongoIP fini client_IDSecret > log
