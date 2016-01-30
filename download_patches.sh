14d9035beba86ec07dc5b10b74dc70d30e6eac98
zouxiaoyu <weiqinzou@qq.com>
2016-01-30 22:42:00 +0800

    yes, we first need to download all patches

---
+++ download_patches.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#example:./download_patches.sh newRepo_closedPR.res
+
+cldPR=$1
+startNum=$2
+endNum=$3
+clientAccount=$4
+patchDir=patch${startNum}-${endNum} #the patch downloaded dir ,such as patches_download
+
+mkdir -p $patchDir 
+clientCnt=$(cat $clientAccount | wc -l)
+cnt=0
+
+rm ${patchDir}/patchFailed.res
+sed -n "$startNum,$endNum p" $cldPR > ./${patchDir}/cldPR
+
+for pr in $(cat ./${patchDir}/cldPR)
+do
+    cnt=$((cnt+1))
+    clientNum=$((cnt%clientCnt + 1))
+    client=$(sed -n "${clientNum}p" $clientAccount)
+
+    fn=$(echo $pr | cut -f1 -d ",")
+    repo=$(echo $fn | cut -f2 -d "/")
+    prID=$(echo $pr | cut -f2 -d ",")
+    #url eg:https://patch-diff.githubusercontent.com/raw/Netflix/servo/pull/370.patch
+    patchURL="https://patch-diff.githubusercontent.com/raw/"${fn}"/pull/"${prID}".patch"
+
+    curl $patchURL -o ./${patchDir}/${repo}_${prID}.patch
+    if [ $? -ne 0 ];then
+        echo $patchURL>>${patchDir}/patchFailed.res
+    fi
+done
