e94dbad35342e2936d151e3b68ddad6b4a550b10
zouxiaoyu <weiqinzou@qq.com>
2016-02-10 07:15:40 +0800

    cor to be done

--- RQ_analysis/pr_csdiff_corr.R
+++ RQ_analysis/pr_csdiff_corr.R
@@ -24,15 +24,29 @@ prepare_expData<-function(t){
     
     txFinal<-cbind(ty,txNor)
     write.table(txFinal,file="final_dataset_csDiff_nor.res",sep=",",row.names=F)
+    return(txFinal)
 
 }
 
 ###todo tommorrow
 corr_cal<-function(tt,corrRes){
-    prL<-tt[,c()]
-    projL<-tt[,c()]
-    submitterL<-tt[,c()]
-    pr_res<-lm.test()
+    prAttr<-c("commit_num","churn_total_size","churn_final_size","churn_max_size","churn_file_num","final_churn_file_num","sameDiff","rmDiff")
+    projAttr<-c("proj_loc","proj_age","proj_star","proj_fork","proj_open_pr","proj_insider","sameDiff","rmDiff")
+    sAttr<-c("schurn_num","sclosedPR_num","scommit_num","sissue_num","smergedPR_num","sclosedPR_avgtime","sameDiff","rmDiff")
+    y<-c("time_close","is_merged","is_insider")
+
+    #print(tt)
+    tt$is_merged<-ifelse(tt$is_merged=="True",1,0)
+    tt$is_insider<-ifelse(tt$is_insider=="True",1,0)
+
+    prL<-tt[,c(prAttr,y)]
+    projL<-tt[,c(projAttr,y)]
+    submitterL<-tt[,c(sAttr,y)]
+
+    print(prL)
+    print(projL)
+    print(submitterL)
+    #pr_res<-lm.test()
 }
 
 t<-merge_csdiff(pr18,csdiff)
