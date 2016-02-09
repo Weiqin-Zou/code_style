f187cba01de82ea59793e35affd39f941ef2bbef
zouxiaoyu <weiqinzou@qq.com>
2016-02-09 20:26:55 +0800

    begin to cal corr tomorrow

--- RQ_analysis/pr_csdiff_corr.R
+++ RQ_analysis/pr_csdiff_corr.R
@@ -9,22 +9,32 @@ merge_csdiff<-function(pr18,csdiff){
     pr<-read.csv(pr18,sep=",",header=T)
     csdiff<-read.csv(csdiff,sep=",",header=T)
     all<-merge(pr,csdiff,by=c("fn","id"))
-    write.table(all,file="final_dataset_csDiff.res",sep=",",row.names=F)
+    write.table(all,file="final_dataset_csDiff_ori.res",sep=",",row.names=F)
     return(all)
 }
 
-t<-merge_csdiff(pr18,csdiff)
-corr_cal<-function(t){
+prepare_expData<-function(t){
     ty<-t[,c(1:3,12,13)]
     tx<-t[,c(4,6:11,14:28)]
+
     tx$sclosedPR_avgtime<-ifelse(tx$sclosedPR_num==0,NA,tx$sclosedPR_avgtime)
     xmax<-apply(tx,2,max,na.rm=T)
     xmin<-apply(tx,2,min,na.rm=T)
-    print(xmax)
-    print(xmin)
     txNor<-t(apply(tx,1,function(x)(x-xmin)/(xmax-xmin)))
-    print(txNor)
+    
     txFinal<-cbind(ty,txNor)
-    print(txFinal)
+    write.table(txFinal,file="final_dataset_csDiff_nor.res",sep=",",row.names=F)
+
 }
-corr_cal(t)
+
+###todo tommorrow
+corr_cal<-function(tt,corrRes){
+    prL<-tt[,c()]
+    projL<-tt[,c()]
+    submitterL<-tt[,c()]
+    pr_res<-lm.test()
+}
+
+t<-merge_csdiff(pr18,csdiff)
+tt<-prepare_expData(t)
+corr_cal(tt,corrRes)
