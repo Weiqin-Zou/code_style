34e82507eed04ce24e849a36744581b1b3e25d9d
zouxiaoyu <weiqinzou@qq.com>
2016-02-11 21:47:15 +0800

    en.corr

--- RQ_analysis/pr_csdiff_corr.R
+++ RQ_analysis/pr_csdiff_corr.R
@@ -3,7 +3,6 @@ argv<-commandArgs(TRUE)
 
 pr18<-argv[1]
 csdiff<-argv[2]
-corrRes<-argv[3]
 
 merge_csdiff<-function(pr18,csdiff){
     pr<-read.csv(pr18,sep=",",header=T)
@@ -28,6 +27,51 @@ prepare_expData<-function(t){
 
 }
 
+get_prLevel_corr<-function(prL){
+##here, we also can use rmDiff to repalce of sameDiff.##
+    prInsider<-tt[prL$is_insider==1,]
+    prOutsider<-tt[prL$is_insider==0,]
+    print("pr level multi logi of is_merged for insider:")
+    prIn_lm<-lm(as.numeric(is_merged)~commit_num+churn_total_size+churn_final_size+churn_max_size+churn_file_num+final_churn_file_num+sameDiff,prInsider)
+    print(summary(prIn_lm))
+
+    print("pr level multi logi of is_merged for outsider:")
+    prOut_lm<-lm(as.numeric(is_merged)~commit_num+churn_total_size+churn_final_size+churn_max_size+churn_file_num+final_churn_file_num+sameDiff,prOutsider)
+    print(summary(prOut_lm))
+
+    print("pr level multi logi of closed_time for insider:")
+    prIn_lm<-lm(as.numeric(time_close)~commit_num+churn_total_size+churn_final_size+churn_max_size+churn_file_num+final_churn_file_num+sameDiff,prInsider)
+    print(summary(prIn_lm))
+
+    print("pr level multi logi of closed_time for outsider:")
+    prOut_lm<-lm(as.numeric(time_close)~commit_num+churn_total_size+churn_final_size+churn_max_size+churn_file_num+final_churn_file_num+sameDiff,prOutsider)
+    print(summary(prOut_lm))
+}
+get_projLevel_corr<-function(projL){
+    projL$is_merged<-as.numeric(projL$is_merged)
+    projIn<-tt[projL$is_insider==1,]
+    projOut<-tt[projL$is_insider==0,]
+
+    projInsider<-ddply(projIn,.(proj_loc,proj_age,proj_star,proj_fork,proj_insider,proj_open_pr),summarize,sameDiffave=mean(sameDiff),rmDiffave=mean(rmDiff),isMerge=length(is_merged[is_merged==1])/length(is_merged),closeTimeave=mean(time_close))
+    print(head(projInsider,20))
+    projOutsider<-ddply(projOut,.(proj_loc,proj_age,proj_star,proj_fork,proj_insider,proj_open_pr),summarize,sameDiffave=mean(sameDiff),rmDiffave=mean(rmDiff),isMerge=length(is_merged[is_merged==1])/length(is_merged),closeTimeave=mean(time_close))
+    write.table(projOutsider,file='out.tmp',sep=',')
+    print("proj level multi logi of is_merged for insider:")
+    projIn_lm<-lm(isMerge~proj_loc+proj_age+proj_star+proj_fork+proj_open_pr+proj_insider+sameDiffave,projInsider)
+    print(summary(projIn_lm))
+
+    print("proj level multi logi of is_merged for outsider:")
+    projOut_lm<-lm(isMerge~proj_loc+proj_age+proj_star+proj_fork+proj_open_pr+proj_insider+sameDiffave,projOutsider)
+    print(summary(projOut_lm))
+
+    print("proj level multi logi of closed_time for insider:")
+    summary(projIn_lm)
+    projIn_lm<-lm(closeTimeave~proj_loc+proj_age+proj_star+proj_fork+proj_open_pr+proj_insider+sameDiffave,projInsider)
+    print(summary(projIn_lm))
+    print("proj level multi logi of closed_time for outsider:")
+    projOut_lm<-lm(closeTimeave~proj_loc+proj_age+proj_star+proj_fork+proj_open_pr+proj_insider+sameDiffave,projOutsider)
+    print(summary(projOut_lm))
+}
 ###todo tommorrow
 corr_cal<-function(tt,corrRes){
     prAttr<-c("commit_num","churn_total_size","churn_final_size","churn_max_size","churn_file_num","final_churn_file_num","sameDiff","rmDiff")
@@ -35,7 +79,6 @@ corr_cal<-function(tt,corrRes){
     sAttr<-c("schurn_num","sclosedPR_num","scommit_num","sissue_num","smergedPR_num","sclosedPR_avgtime","sameDiff","rmDiff")
     y<-c("time_close","is_merged","is_insider")
 
-    #print(tt)
     tt$is_merged<-ifelse(tt$is_merged=="True",1,0)
     tt$is_insider<-ifelse(tt$is_insider=="True",1,0)
 
@@ -43,10 +86,10 @@ corr_cal<-function(tt,corrRes){
     projL<-tt[,c(projAttr,y)]
     submitterL<-tt[,c(sAttr,y)]
 
-    print(prL)
-    print(projL)
-    print(submitterL)
-    #pr_res<-lm.test()
+    #get_prLevel_corr(prL)
+    get_projLevel_corr(projL)
+    #get_submitterLevel_corr(submitterL)
+
 }
 
 t<-merge_csdiff(pr18,csdiff)
